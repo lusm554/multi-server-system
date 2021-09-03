5a302dd5cc3e7e389589a508fede753a6411992e
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-04 01:52:40 +0300

    removed db

--- src/app-server/src/index.py
+++ src/app-server/src/index.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, render_template, request as req
 from flask_cors import CORS
 from blueprint_module import users_bp
-import psycopg2
-from env_config import DB_USERNAME, DB_PWD, DB_HOSTNAME, DB_PORT, DB_NAME
 
 app = Flask(__name__,
             static_url_path='',
@@ -14,3 +12,4 @@ CORS(app) # for testing endpoints
 @app.route('/')
 def get_main():
     return render_template('index.html')
+
