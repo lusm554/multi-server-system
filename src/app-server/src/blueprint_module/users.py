5a5a4495c3853feca8ca18b07131f7602db636ab
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-04 20:01:49 +0300

    try catch, handle errors

--- src/app-server/src/blueprint_module/users.py
+++ src/app-server/src/blueprint_module/users.py
@@ -5,8 +5,12 @@ bp = Blueprint('users', __name__, url_prefix='/user')
 
 @bp.route('/')
 def hello():
-    cur = conn.cursor()
-    cur.execute('select * from test')
-    rows = cur.fetchall()
-    cur.close()
-    return jsonify(rows)
+    try:
+        cur = conn.cursor()
+        cur.execute('select * from test')
+        rows = cur.fetchall()
+        cur.close()
+        return jsonify(rows)
+    except Exception as e:
+        print(e)
+        return 'Error while fetching users', 500
