3f9b9dc3de86af86e2545f28ac49009f0fb0ec14
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-05 22:33:49 +0300

    added prefix for api routes

--- src/app-server/src/index.py
+++ src/app-server/src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request as req
+from flask import Flask, Blueprint, render_template, request as req
 from flask_cors import CORS
 from src.routes import users_route 
 
@@ -6,7 +6,13 @@ app = Flask(__name__,
             static_url_path='',
             template_folder='static',
             static_folder='static')
-app.register_blueprint(users_route)
+
+# add prefix '/api/v1' for routes registered in
+api_bp = Blueprint('api-v1', __name__, url_prefix='/api/v1')
+api_bp.register_blueprint(users_route)
+
+# add API routes
+app.register_blueprint(api_bp)
 CORS(app) # for testing endpoints
 
 @app.route('/')
