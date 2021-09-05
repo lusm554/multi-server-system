d9240284420af1b10368852e39c5b151dc291b72
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-05 16:15:19 +0300

    script for creating db if not exists

--- src/app-server/src/db.py
+++ src/app-server/src/db.py
@@ -1,5 +1,18 @@
 import psycopg2
 from src.env_config import DB_USERNAME, DB_PWD, DB_HOSTNAME, DB_PORT, DB_NAME
+from src.managedb import ManageDB
 
 conn_url = f'postgresql://{DB_USERNAME}:{DB_PWD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
-conn = psycopg2.connect(conn_url)
+
+try:
+    conn = psycopg2.connect(conn_url)
+except Exception as main_conn_err:
+    if not str(main_conn_err).startswith(f'FATAL:  database "{DB_NAME}" does not exist'):
+        raise main_conn_err
+    try:
+        print(f'Database {DB_NAME} does not exist, creating...')
+        conn = psycopg2.connect(conn_url[0:len(conn_url) - len(DB_NAME)])
+        ManageDB(conn).setupDatabase()
+    except Exception as sub_conn_err:
+        raise sub_conn_err
+
---
+++ src/app-server/src/managedb.py
@@ -0,0 +1,15 @@
+import psycopg2
+from os import path
+
+class ManageDB:
+    def __init__(self, conn):
+        self.conn = conn
+        self.__sql_dir__ = path.join(path.dirname(path.abspath(__file__)), 'sql')
+
+    def setupDatabase(self):
+        try:
+            cur = self.conn.cursor()
+            cur.execute(open(path.join(self.__sql_dir__, 'setup_db.sql'), 'r').read())
+            print('The database is built.')
+        except Exception as e:
+            raise e
