d8b781c45768bc014de07b09c2552a446427f345
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-06 14:23:42 +0300

    created signup api endpoint

---
+++ src/app-server/src/dao/users.py
@@ -0,0 +1,35 @@
+import psycopg2
+from datetime import datetime, timezone
+from src.db import conn
+
+def __is_exist__(table, row, val):
+    cur = conn.cursor()
+    query = 'select exists (           \
+        select 1 from {} where {} = %s \
+    )'
+    cur.execute(query.format(table, row), (val,))
+    isexist = cur.fetchone()[0]
+    cur.close()
+    return isexist
+
+
+class UsersDAO:
+    def isUserExist(name):
+        return __is_exist__('users', 'username', name)
+
+
+    def add(username, password):
+        created_at = updated_at = datetime.now(timezone.utc)
+        cur = conn.cursor()
+        query = 'insert into users (username, password, created_at, updated_at) \
+                values (%s, %s, %s::timestamp, %s::timestamp);'
+        cur.execute(query, (username, password, created_at, updated_at))
+        conn.commit()
+        '''
+        cur.execute('select * from users where username = %s', (username,))
+        return cur.fetchone()
+        '''
+        
+
+    #def get(username, password):
+        
--- src/app-server/src/routes/users.py
+++ src/app-server/src/routes/users.py
@@ -1,16 +1,38 @@
-from flask import Blueprint, jsonify
-from src.db import conn
+from functools import wraps
+from flask import (
+    Blueprint, 
+    jsonify, 
+    Response, 
+    request as req
+)
+from src.dao.users import UsersDAO
 
-router = Blueprint('users', __name__, url_prefix='/user')
+router = Blueprint('users', __name__, url_prefix='/users')
 
-@router.route('/')
-def hello():
+def validate_signup_json(f):
+    @wraps(f)
+    def wrapper(*args, **kw):
+        try:
+            assert req.is_json
+            data = req.get_json()
+            assert 'username' in data and 'password' in data
+        except Exception as e:
+            msg = 'payload must be a valid json'
+            return jsonify({"error": msg}), 400
+        return f(*args, **kw)
+    return wrapper
+
+@router.route('/', methods=['POST'])
+@validate_signup_json
+def signup():
     try:
-        cur = conn.cursor()
-        cur.execute('select * from users')
-        rows = cur.fetchall()
-        cur.close()
-        return jsonify(rows)
+        data = req.get_json()
+        if UsersDAO.isUserExist(data['username']):
+            return Response(status=409)
+        UsersDAO.add(**data)     
+        return Response(status=201)
     except Exception as e:
         print(e)
-        return 'Error while fetching users', 500
+        srv_err_code = Response(status=500)
+        return srv_err_code
+
