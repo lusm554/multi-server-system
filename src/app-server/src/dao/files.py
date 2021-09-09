75a99c9a9a3fe8859a321c4657da7b926f9c3023
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-09 18:44:36 +0300

    manage chunks & objects

--- src/app-server/src/dao/files.py
+++ src/app-server/src/dao/files.py
@@ -24,3 +24,32 @@ class FilesDAO(DAO):
                 '
         data = (user_id, object_type, parent_object_id, name, created_at, updated_at)
         return self.__perform_db_req__(query, data)
+
+    def getChunk(self, object_id, url):
+        query = 'select * from chunks               \
+                 where object_id = %s and url = %s  \
+                '
+        return self.__perform_db_req__(query, (object_id, url))
+
+    def getObject(self, user_id, name):
+        query = 'select * from objects              \
+                 where                              \
+                    user_id = %s and name = %s and  \
+                    parent_object_id is null        \
+                '
+        data = self.__perform_db_req__(query, (user_id, name), with_description=True)
+        colnames = [desc[0] for desc in data['desc']]
+        return {key: val for key, val in zip(colnames, data['content'])}
+
+    def createChunk(self, object_id, url):
+        created_at = updated_at = datetime.now(timezone.utc)
+        query = 'insert into chunks (    \
+                    object_id,           \
+                    url,                 \
+                    created_at,          \
+                    updated_at           \
+                 )                       \
+                values (%s, %s, %s, %s); \
+                '
+        self.__perform_db_req__(query, (object_id, url, created_at, updated_at))
+        return self.getChunk(object_id, url)
