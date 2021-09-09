3e345e3ed2273c12b1067a469b340a8a9ee0b98f
loveyousomuch <loveyousomuch554@gmail.com>
2021-09-09 18:43:02 +0300

    save file from user

--- src/block-storage-server/src/manage_dir.py
+++ src/block-storage-server/src/manage_dir.py
@@ -13,3 +13,6 @@ class ManageDir:
             print(f'Dir {self.dir_name} created.')
         except OSError as err:
             return err
+
+    def save(self, file, name):
+        file.save(os.path.join(self.dir_name, name))
--- src/block-storage-server/src/routes/files.py
+++ src/block-storage-server/src/routes/files.py
@@ -3,12 +3,29 @@ from flask import (
     jsonify,
     request as req
 )
+from src.manage_dir import ManageDir
+Dir = ManageDir()
 router = Blueprint('files', __name__, url_prefix='/chunks')
+# TODO: create opportunity to save files in user dirs
 
-
-@router.route('/')
-def get():
+@router.route('/<chunk_url>')
+def get_chunk(chunk_url):
     try:
         return 'OK', 200
-    except:
-        return Resposne(status=500)
+    except Exception as e:
+        print(e)
+        return '', 500
+
+@router.route('/<chunk_url>', methods=['POST'])
+def add_chunk(chunk_url):
+    try:
+        print(req.files)
+        if 'file' not in req.files:
+            return '', 400
+        file = req.files['file']
+        if file.filename == '':
+            return '', 400
+        Dir.save(file, chunk_url)
+        return 'OK', 200
+    except Exception as e:
+        print(e)
